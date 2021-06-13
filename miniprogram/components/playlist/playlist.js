e0aae208b3f1d7d1ff634ab195aa09f49e29b017
Irelia <yaokai729@outlook.com>
2021-06-13 23:14:07 +0800

    Initial Commit

---
+++ miniprogram/components/playlist/playlist.js
@@ -0,0 +1,33 @@
+Component({
+  properties: {
+    playlist: {
+      type: Object
+    }
+  },
+  observers: {  // 监听属性变化（watch）
+    ['playlist.playCount'](count) {
+      console.log(count);
+      console.log(this._tranNumber(count, 2));
+      this.setData({
+        _count: this._tranNumber(count, 2)
+      })
+    }
+  },
+  data: {
+    _count: 0
+  },
+  methods: {
+    _tranNumber(num, point) {
+      let numStr = num.toString().split('.')[0]
+      if (numStr.length < 6) {
+        return numStr
+      } else if (numStr.length >= 6 && numStr.length <= 8) {
+        let decimal = numStr.substring(numStr.length - 4, numStr.length - 4 + point)
+        return parseFloat(parseInt(numStr / 10000) + '.' + decimal) + '万'
+      }else if(numStr.length > 8){
+        let decimal = numStr.substring(numStr.length - 8, numStr.length - 8 + point)
+        return parseFloat(parseInt(numStr / 100000000) + '.' + decimal) + '亿'
+      }
+    }
+  }
+})
