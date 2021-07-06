b7e2f3dc9b709bad3b18a1b8256bc7e31e1c8636
Irelia <yaokai729@outlook.com>
2021-07-06 23:33:18 +0800

    '完成博客编辑页面'

--- miniprogram/pages/blog/blog.js
+++ miniprogram/pages/blog/blog.js
@@ -5,9 +5,46 @@ Page({
    * 页面的初始数据
    */
   data: {
+    modalShow: false
+  },
+  onPublish() {
+    // this.setData({
+    //   modalShow: true,
+    // })
+    wx.getSetting({
+      success: res => {
+        console.log(res);
+        if (res.authSetting['scope.userInfo']) {
+          wx.getUserInfo({
+            success: res => {
+              console.log(res);
+              this.onLoginSuccess({
+                detail: res.userInfo
+              })
+            }
+          })
+        } else {
+          this.setData({
+            modalShow: true,
+          })
+        }
+      }
+    })
 
   },
-
+  onLoginSuccess(event) {
+    console.log(event)
+    const detail = event.detail
+    wx.navigateTo({
+      url: `../blog-edit/blog-edit?nickName=${detail.nickName}&avatarUrl=${detail.avatarUrl}`,
+    })
+  },
+  onLoginFail() {
+    wx.showModal({
+      title: '授权用户才能发布',
+      content: '',
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
